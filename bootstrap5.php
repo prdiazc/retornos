fed196572ac52a22dc2fe0f3a7308275a9fbadcf
pdiaz <pdiaz@soyucn.edu.co>
2021-12-17 18:54:17 -0500

    Se agrega el proyecto

---
+++ bootstrap5.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+
+        <!--
+        <link rel="stylesheet" href="css/bootstrap.css" >
+    
+        -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" >
+        <link rel="stylesheet" href="css/bootstrap.rtl.css" >
+        <link rel="stylesheet" href="css/bootstrap.rtl.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-grid.css" >
+        <link rel="stylesheet" href="css/bootstrap-grid.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-grid.rtl.css" >
+        <link rel="stylesheet" href="css/bootstrap-grid.rtl.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-reboot.css" >
+        <link rel="stylesheet" href="css/bootstrap-reboot.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-reboot.rtl.css" >
+        <link rel="stylesheet" href="css/bootstrap-reboot.rtl.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-utilities.css" >
+        <link rel="stylesheet" href="css/bootstrap-utilities.min.css" >
+        <link rel="stylesheet" href="css/bootstrap-utilities.rtl.css" >
+        <link rel="stylesheet" href="css/bootstrap-utilities.rtl.min.css" >
+
+
+        <!--<script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!--<script src="js/bootstrap.esm.js"></script>
+        <script src="js/bootstrap.esm.min.js"></script>-->
+        <!--<script src="js/bootstrap.js"></script>-->
+        <script src="js/bootstrap.min.js"></script>
+
+    </head>
+</hthml>
